16df6d77afd77dd81e852765c257250d73953c25
Bluemo <shutaro.aoyama@gmail.com>
2022-03-21 10:31:05 +0900

    Installed Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: `${process.env.React_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    projectId: process.env.React_APP_FIREBASE_PROJECT_ID,
+    storageBucket: `${process.env.React_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.React_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.React_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
