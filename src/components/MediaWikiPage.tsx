8da7e18852c9169426186973625e25b6082c2876
Bluemo <shutaro.aoyama@gmail.com>
2022-11-18 00:43:50 -0500

    Changed import method of Wikipedia stylesheets

--- src/components/MediaWikiPage.tsx
+++ src/components/MediaWikiPage.tsx
@@ -1,5 +1,4 @@
 import React, {createElement, MouseEventHandler, useEffect, useState} from "react";
-import "./MediaWikiPage.css"
 import useMediaWikiPage from "../hooks/useMediaWikiPage";
 
 type Props = {
@@ -14,17 +13,20 @@ function MediaWikiPage(props: Props) {
     const [pageElement, redirectedTitle, tagLine] = useMediaWikiPage(props.apiURL, props.title, props.onLinkClick, props.hideBottomContent);
 
     return (
-        <div className="mediaWikiPage">
-            <h1 id="firstHeading" className="firstHeading mw-first-heading">{props.title}</h1>
-            <div id="bodyContent" className="vector-body">
-                <div id="siteSub" className="noprint">{tagLine}</div>
-                <div id="contentSub"></div>
-                <div id="contentSub2"></div>
-                <div id="mw-content-text" className="mw-body-content mw-content-ltr" lang="ja" dir="ltr">
-                    {pageElement !== "" ? pageElement : "Loading..."}
+        <>
+            <link rel={"stylesheet"} type={"text/css"} href={"https://mediawiki.org/w/load.php?modules=skins.vector.styles&only=styles"}/>
+            <div className="mediaWikiPage">
+                <h1 id="firstHeading" className="firstHeading mw-first-heading">{props.title}</h1>
+                <div id="bodyContent" className="vector-body">
+                    <div id="siteSub" className="noprint">{tagLine}</div>
+                    <div id="contentSub"></div>
+                    <div id="contentSub2"></div>
+                    <div id="mw-content-text" className="mw-body-content mw-content-ltr" lang="ja" dir="ltr">
+                        {pageElement !== "" ? pageElement : "Loading..."}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 
 }
