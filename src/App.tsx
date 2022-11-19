0e498d748751eb75b6a9a3027023a1c9ff49151e
Bluemo <shutaro.aoyama@gmail.com>
2022-11-19 02:00:25 -0500

    Added title and meta description

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { useGame } from "./hooks/game/useGame";
 import { BrowserRouter, Routes, Route, useParams} from 'react-router-dom';
 import Game from "./components/Game";
 import Lobby from "./components/Lobby";
+// @ts-ignore
+import { Helmet } from "react-helmet";
 
 function App() {
 
@@ -22,6 +24,10 @@ function App() {
 
     return (
         <BrowserRouter>
+            <Helmet>
+                <title>Wikichase | Manhunt Game on Wikipedia</title>
+                <meta name="description" content="Manhunt game on Wikipedia. Hop around pages by clicking links. " />
+            </Helmet>
             <Routes>
                 <Route
                     path="/"
