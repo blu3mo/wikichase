f95745ca97cf07d6c779676ea1b6646334d32912
Bluemo <shutaro.aoyama@gmail.com>
2022-11-23 21:18:58 -0500

    Supported React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -18,12 +19,13 @@ const firebaseConfig = {
 // Initialize Firebase
 initializeApp(firebaseConfig);
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = ReactDOMClient.createRoot(container!)
+root.render(
     <React.StrictMode>
         <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+    </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
